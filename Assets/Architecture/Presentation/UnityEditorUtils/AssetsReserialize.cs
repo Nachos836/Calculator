3adea8c58f6775be2295bc4ea8875ab1c0474d21
Cyka <nachos636@gmail.com>
2024-07-02 13:54:53 +0300

    [CALC-3] [ADD] AssetsReserialize Editor-related feature

---
+++ Assets/Architecture/Presentation/UnityEditorUtils/AssetsReserialize.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Calc.Architecture.Presentation.UnityEditorUtils
+{
+	internal static class AssetsReserialize
+    {
+        [MenuItem("Build/Force Reserialize Assets")]
+        private static void ForceReserializeAssets()
+        {
+            AssetDatabase.ForceReserializeAssets();
+        }
+    }
+}
