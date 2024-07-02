c9e11232467c0a80218f6b06007fb5da1fdc2b7f
Cyka <nachos636@gmail.com>
2024-07-02 13:54:13 +0300

    [CALC-3] [ADD] Third-Party dependencies
    add: Roslyn libraries

---
+++ Assets/ThirdParty/Roslyn/DoNotUseDefaultConstructionAttribute.cs
@@ -0,0 +1,6 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.ComponentModel.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Struct)]
+    public sealed class DoNotUseDefaultConstructionAttribute : Attribute { }
+}
