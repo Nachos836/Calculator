20f465e99a7bb6688b8d083a73c26cbad253d37c
Sergey Shevchuk <nachos636@gmail.com>
2024-07-03 20:00:46 +0300

    [CALC-5] [ADD] DI: Presentation Scope

---
+++ Assets/Architecture/Presentation/DI/Presentation/PresentationScope.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using VContainer.Unity;
+
+namespace Calc.Architecture.Presentation.DI.Presentation
+{
+    [AddComponentMenu("Game/Architecture/Presentation Scope")]
+    [DisallowMultipleComponent]
+    internal sealed class PresentationScope : LifetimeScope
+    {
+
+    }
+}
