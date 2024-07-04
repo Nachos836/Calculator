c1b5610d22e39876cdee7c39caa141f37888455f
Sergey Shevchuk <nachos636@gmail.com>
2024-07-04 19:55:06 +0300

    [CALC-7] [ADD] Operand Value Object

---
+++ Assets/Architecture/Domain/Operation/Operand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Calc.Domain.Functional;
+
+namespace Calc.Domain.Operation
+{
+    public readonly record struct UnsignedOperand
+    {
+        public decimal Raw { get; }
+
+        public static Result<UnsignedOperand> Create(decimal income)
+        {
+            if (income < decimal.Zero)
+            {
+                return Result<UnsignedOperand>
+                    .FromException(new Exception($"Value should not be negative! Value is {income}"));
+            }
+
+            var candidate = decimal.Round(income);
+            if (candidate != income)
+            {
+                return Result<UnsignedOperand>
+                    .FromException(new Exception($"Value should not be fractional! Value is {income}"));
+            }
+
+            return new UnsignedOperand(income);
+        }
+
+        private UnsignedOperand(decimal income)
+        {
+            Raw = income;
+        }
+    }
+}
---
+++ Assets/Architecture/Domain/Tests/UnitTests/Operand_UnitTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Calc.Architecture.Domain.Tests.UnitTests
+{
+    using Calc.Domain.Operation;
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    internal sealed class Operand_UnitTests
+    {
+        [Test]
+        public void OperandWithValue1_0mDecimalCreated_Passes()
+        {
+            var value = 1.0m;
+            var candidate = UnsignedOperand.Create(value);
+
+            candidate.Match
+            (
+                success: stored => Assert.AreEqual(value, stored.Raw),
+                error: static exception => Assert.Fail(exception.Message)
+            );
+        }
+
+        [Test]
+        public void OperandWithValue1_9999999mDecimalCreated_Fails()
+        {
+            var value = 1.999999m;
+            var candidate = UnsignedOperand.Create(value);
+
+            candidate.Match
+            (
+                success: static _ => Assert.Fail("Should not happen!"),
+                error: static exception => Assert.Pass(exception.Message)
+            );
+        }
+
+        [Test]
+        public void OperandWithValueMinus1_0mDecimalCreated_Fails()
+        {
+            var value = decimal.MinusOne;
+            var candidate = UnsignedOperand.Create(value);
+
+            candidate.Match
+            (
+                success: static _ => Assert.Fail("Should not happen!"),
+                error: static exception => Assert.Pass(exception.Message)
+            );
+        }
+
+        [Test]
+        public void OperandWithMaxDecimalValueCreated_Passes()
+        {
+            var value = decimal.MaxValue;
+            var candidate = UnsignedOperand.Create(value);
+
+            candidate.Match
+            (
+                success: stored => Assert.AreEqual(value, stored.Raw),
+                error: static exception => Assert.Fail(exception.Message)
+            );
+        }
+
+        [Test]
+        public void OperandWithMinDecimalValueCreated_Fails()
+        {
+            var value = decimal.MinValue;
+            var candidate = UnsignedOperand.Create(value);
+
+            candidate.Match
+            (
+                success: static _ => Assert.Fail("Should not happen!"),
+                error: static exception => Assert.Pass(exception.Message)
+            );
+        }
+    }
+}
