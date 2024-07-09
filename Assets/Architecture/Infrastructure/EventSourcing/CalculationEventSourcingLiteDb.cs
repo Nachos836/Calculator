b26de54dd34a634b67b043d23fb5fd3f5fba9ebb
Sergey Shevchuk <nachos636@gmail.com>
2024-07-09 07:13:39 +0300

    [CALC-15] [HOTFIX] Resolve inconsistent ICalculationEventsSourcing API

--- Assets/Architecture/Infrastructure/EventSourcing/CalculationEventSourcingLiteDb.cs
+++ Assets/Architecture/Infrastructure/EventSourcing/CalculationEventSourcingLiteDb.cs
@@ -23,6 +23,8 @@ namespace Calc.Infrastructure.EventSourcing
             if (@event is ClosedEvent)
             {
                 _collection.DeleteAll();
+
+                return;
             }
 
             _collection.Insert(@event);
@@ -30,19 +32,32 @@ namespace Calc.Infrastructure.EventSourcing
 
         IEnumerable<string> ICalculationEventsSourcing.MaterializeView(Guid session)
         {
-            return _collection.Find(@event => @event.StreamId == session)
+            using var events = _collection.Find(@event => @event.StreamId == session)
                 .OrderBy(static @event => @event.CreatedTimestamp)
-                .Select(static @event => @event.RawView);
+                .GetEnumerator();
+
+            if (events.MoveNext() is false) yield break;
+            if (events.Current is ClosedEvent)
+            {
+                _collection.DeleteAll();
+
+                yield break;
+            }
+
+            while (events.MoveNext())
+            {
+                yield return events.Current!.RawView;
+            }
         }
 
-        string ICalculationEventsSourcing.GetLatestFromMaterializedView(Guid session)
+        string? ICalculationEventsSourcing.GetLatestFromMaterializedView(Guid session)
         {
             var latest = _collection.Find(@event => @event.StreamId == session)
                 .OrderByDescending(static @event => @event.CreatedTimestamp)
                 .FirstOrDefault();
-            return latest is not null and not ClosedEvent
-                ? latest.RawView
-                : string.Empty;
+            return latest is not ClosedEvent
+                ? latest?.RawView
+                : null;
         }
 
         void IDisposable.Dispose()
